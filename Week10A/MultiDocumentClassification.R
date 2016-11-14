33f51d68d278ede9d11fdb4ac8a44c9516d1e869
DataDriven-MSDA <kumudini.bhave@gmail.com>
2016-11-14 17:15:50 -0500

    Add files via upload

--- Week10A/MultiDocumentClassification.R
+++ Week10A/MultiDocumentClassification.R
@@ -19,9 +19,12 @@ category <- c("grain","wheat")
 
 trade.directory <- "F:/Data/Reuters21578_90Cat/training/trade"
 moneyfx.directory <- "F:/Data/Reuters21578_90Cat/training/moneyfx"    
+crude.directory <- "F:/Data/Reuters21578_90Cat/training/crude"    
+
 
 tradetest.dir <- "F:/Data/Reuters21578_90Cat/test/trade"
 moneyfxtest.dir <- "F:/Data/Reuters21578_90Cat/test/moneyfx"
+crudetest.dir <- "F:/Data/Reuters21578_90Cat/test/crude"
 
 
 
@@ -47,50 +50,69 @@ cleanupDataCorpus <- function(dataCorpus){
 
 trade.corpus <- Corpus(DirSource(directory=trade.directory, encoding = "ASCII"))
 moneyfx.corpus <- Corpus(DirSource(directory=moneyfx.directory, encoding = "ASCII"))
+crude.corpus <- Corpus(DirSource(directory=crude.directory, encoding = "ASCII"))
+
 
 trade.cleancorpus <- cleanupDataCorpus(trade.corpus)
 moneyfx.cleancorpus <- cleanupDataCorpus(moneyfx.corpus)
+crude.cleancorpus <- cleanupDataCorpus(crude.corpus)
 
 trade.tdm <- TermDocumentMatrix(trade.cleancorpus)
 moneyfx.tdm <- TermDocumentMatrix(moneyfx.cleancorpus)
+crude.tdm <- TermDocumentMatrix(crude.cleancorpus)
+
 
 trade.tdm <- removeSparseTerms(trade.tdm, 0.7)
 moneyfx.tdm<- removeSparseTerms(moneyfx.tdm, 0.7)
+crude.tdm<- removeSparseTerms(crude.tdm, 0.7)
 
 trade.datamatx <- t(data.matrix(trade.tdm))
 moneyfx.datamatx <- t(data.matrix(moneyfx.tdm))
+crude.datamatx <- t(data.matrix(crude.tdm))
+
 
 head(trade.datamatx)
 head(moneyfx.datamatx)
+head(crude.datamatx)
+
 
 
 trade.df <- as.data.frame(trade.datamatx)
 moneyfx.df <- as.data.frame(moneyfx.datamatx)
+crude.df <- as.data.frame(crude.datamatx)
+
 
 # build Term Document Matrix for test data
 
 tradetest.corpus <- Corpus(DirSource(directory=tradetest.dir, encoding = "ASCII"))
 moneyfxtest.corpus <- Corpus(DirSource(directory=moneyfxtest.dir, encoding = "ASCII"))
+crudetest.corpus <- Corpus(DirSource(directory=crudetest.dir, encoding = "ASCII"))
+
 
 tradetest.cleancorpus <- cleanupDataCorpus(tradetest.corpus)
 moneyfxtest.cleancorpus <- cleanupDataCorpus(moneyfxtest.corpus)
+crudetest.cleancorpus <- cleanupDataCorpus(crudetest.corpus)
+
 
 tradetest.tdm <- TermDocumentMatrix(tradetest.cleancorpus)
 moneyfxtest.tdm <- TermDocumentMatrix(moneyfxtest.cleancorpus)
+crudetest.tdm <- TermDocumentMatrix(crudetest.cleancorpus)
 
 tradetest.tdm <- removeSparseTerms(tradetest.tdm, 0.7)
 moneyfxtest.tdm<- removeSparseTerms(moneyfxtest.tdm, 0.7)
+crudetest.tdm<- removeSparseTerms(crudetest.tdm, 0.7)
 
 tradetest.datamatx <- t(data.matrix(tradetest.tdm))
 moneyfxtest.datamatx <- t(data.matrix(moneyfxtest.tdm))
+crudetest.datamatx <- t(data.matrix(crudetest.tdm))
 
 head(tradetest.datamatx)
 head(moneyfxtest.datamatx)
-
+head(crudetest.datamatx)
 
 tradetest.df <- as.data.frame(tradetest.datamatx)
 moneyfxtest.df <- as.data.frame(moneyfxtest.datamatx)
-
+crudetest.df <- as.data.frame(crudetest.datamatx)
 
 # filtering for columns present in test and training and then adding the columns/terms present only in training
 # attempt to keep the same terms in test and training 
@@ -133,16 +155,35 @@ ncol(moneyfxtestfiltered)
 
 
 
+# subset crude testing data by colnames (ie. terms) from crude training data
+
+
+crude.df.mat <- as.matrix(crude.df)
+crudetest.df.mat <-  as.matrix(crudetest.df)
+
+
+crudetestfiltered1 <- data.frame(crudetest.df.mat[,intersect(colnames(crudetest.df.mat),
+                                                                 colnames(crude.df.mat))])
+
+crudetestfiltered2 <- read.table(textConnection(""), col.names = colnames(crude.df.mat))
+
+
+crudetestfiltered <- rbind.fill(crudetestfiltered1, crudetestfiltered2)
+
+ncol(crudetestfiltered)
+
 
 
 # Binding 'Category' column to the data frame
 
 trade.df <- cbind(trade.df, category= rep("trade"))
 moneyfx.df <- cbind(moneyfx.df, category = rep("moneyfx"))
+crude.df <- cbind(crude.df, category = rep("crude"))
+
 
 tradetestfiltered <- cbind(tradetestfiltered, category= rep("trade"))
 moneyfxtestfiltered <- cbind(moneyfxtestfiltered, category = rep("moneyfx"))
-
+crudetestfiltered <- cbind(crudetestfiltered, category = rep("crude"))
 
 # We now have two dataframes  , one each category , with the content as rows and the terms as variables / columns.
 # and a category column appended to denote the category.
@@ -150,29 +191,33 @@ moneyfxtestfiltered <- cbind(moneyfxtestfiltered, category = rep("moneyfx"))
 
 # Stacking the TDMS of different categories for training data
 
-tdm.stackedcategory <- rbind.fill(trade.df, moneyfx.df)
+tdm.stackedcategory <- rbind.fill(trade.df, moneyfx.df, crude.df)
 
 nrow(trade.df)
 nrow(moneyfx.df)
+nrow(crude.df)
 nrow(tdm.stackedcategory)
 
 tdm.stackedcategory [is.na(tdm.stackedcategory)] <- 0
 
 ncol(trade.df)
 ncol(moneyfx.df)
+ncol(crude.df)
 ncol(tdm.stackedcategory)
 
 # Stacking the TDMS of different categories for test data
-tdm.teststackedcategory <- rbind.fill(tradetestfiltered, moneyfxtestfiltered)
+tdm.teststackedcategory <- rbind.fill(tradetestfiltered, moneyfxtestfiltered, crudetestfiltered)
 
 nrow(tradetestfiltered)
 nrow(moneyfxtestfiltered)
+nrow(crudetestfiltered)
 nrow(tdm.teststackedcategory)
 
 tdm.teststackedcategory [is.na(tdm.teststackedcategory)] <- 0
 
 ncol(tradetestfiltered)
 ncol(moneyfxtestfiltered)
+ncol(crudetestfiltered)
 ncol(tdm.teststackedcategory) 
 
 
@@ -216,9 +261,9 @@ error <- 1 - accuracy
 
 
 
-container <- create_container(tdm.train, tdm.stackedcategory$category, trainSize=1:907, virgin=FALSE)
+container <- create_container(tdm.train, tdm.stackedcategory$category, trainSize=1:1296, virgin=FALSE)
 
-predtestcontainer <- create_container(tdm.test, labels= 0:296, testSize = 1:296, virgin=FALSE)
+predtestcontainer <- create_container(tdm.test, labels= 0:485, testSize = 1:485, virgin=FALSE)
 
 
 # MODEL SVM
@@ -258,7 +303,7 @@ results.TREE
 
 
 cat_check <- data.frame(
-     correct_cat = tdm.testcategorycol[1:296],
+     correct_cat = tdm.testcategorycol[1:485],
      svm = as.character(results.SVM[,1]),
      maxent = as.character(results.MAXENT[,1]),
      bagging = as.character(results.BAGGING[,1]),
@@ -301,7 +346,6 @@ ptree <- prop.table(tree_table)
 ptree <-as.data.frame(ptree)
 ptree$Model <- "TREE"
 
-
 pmodels <- rbind.fill(as.data.frame(psvm)
 ,as.data.frame(pmaxent)
 ,as.data.frame(pbag)
@@ -319,7 +363,7 @@ datatable(pmodels)
 
 
 
-ggplot(pmodels, aes(x=Model, y=Frequency)) + scale_y_discrete(labels=seq(0, 1, 0.1)) + ggtitle("Model Performance Plot") + geom_point(aes(colour = Status), size = 5) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggplot(pmodels, aes(Model, Frequency)) +     geom_point(aes(colour = Status), size = 5) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
 
@@ -340,6 +384,7 @@ ggplot(pmodels, aes(x=Model, y=Frequency)) + scale_y_discrete(labels=seq(0, 1, 0
 #wordcloud(trade.cleancorpus, max.words =20 , random.order = FALSE, colors = brewer.pal(8,"Dark2"),  scale = c(4,0.8), vfont = c("gothic english","plain"))
 #wordcloud(moneyfx.cleancorpus,max.words =20 , random.order = FALSE,colors = brewer.pal(9,"Reds") ,  scale = c(3,0.5), vfont=c("serif","plain"))
 
+#wordcloud(crude.cleancorpus,max.words =20 , random.order = FALSE,colors = brewer.pal(9,"Reds") ,  scale = c(3,0.5), vfont=c("serif","plain"))
 
 
 colnames(moneyfx.df)
